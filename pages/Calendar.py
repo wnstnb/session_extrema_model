edbf26e6ff8d8d0d940dcc925145a93335792600
Winston Bandong <w.bandong@gmail.com>
2024-01-18 21:21:44 -0800

    fixing calendar spacing

--- pages/Calendar.py
+++ pages/Calendar.py
@@ -10,7 +10,7 @@ st.title('📆 Economic Calendar')
 
 # HTML code
 html_code = """
-<iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&importance=2,3&features=datepicker,timezone,filters&countries=5&calType=day&timeZone=8&lang=1" width="650" height="750" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
+<iframe src="https://sslecal2.investing.com?columns=exc_flags,exc_currency,exc_importance,exc_actual,exc_forecast,exc_previous&importance=2,3&features=datepicker,timezone,filters&countries=5&calType=day&timeZone=8&lang=1" style="width:100vw; height:100vh;" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe>
 <div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">
     <span style="font-size: 11px;color: #333333;text-decoration: none;">Real Time Economic Calendar provided by 
         <a href="https://www.investing.com/" rel="nofollow" target="_blank" style="font-size: 11px;color: #06529D; font-weight: bold;" class="underline_link">Investing.com</a>.
